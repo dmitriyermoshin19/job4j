37e88b7ff51498b02e10c9529a1c24a4239a59b7
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-10-09 14:35:09 +0300

    4.1. Глупый бот.[#184726]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+        public static String answer(String question) {
+            String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+            String question1 = "Привет, Бот.";
+            String question2 = "Пока.";
+            if (question1.equals(question)) {
+                rsl = "Привет, умник.";
+            } else if (question2.equals(question)) {
+                rsl = "До скорой встречи.";
+            }
+            return rsl;
+        }
+
+        public static void main(String[] args) {
+            DummyBot Bot = new DummyBot();
+            String myquestion = "Привет, Бот.";
+            String answer = Bot.answer(myquestion);
+            System.out.println(answer);
+        }
+
+}
