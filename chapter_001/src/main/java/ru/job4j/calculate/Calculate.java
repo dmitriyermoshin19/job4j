7cc5688c7217b3499b8857b26b761c81dabca843
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-10-15 13:39:30 +0300

    5.8. JavaDoc. Code convention[#184740]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,18 @@
+
+
+package ru.job4j.calculate;
+
+/**
+ * Calculate
+ *
+ * @author Dmitriy Ermoshin
+ */
+public class Calculate {
+    /**
+     * Method main.
+     * @param args - args.
+     */
+    public static void main(String [] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/packageInfo.java
@@ -0,0 +1,11 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Dmitriy Ermoshin
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
+
+public class packageInfo {
+}
