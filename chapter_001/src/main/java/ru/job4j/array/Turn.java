9e8fd3a092f7ddc6504cb50ee65329aa9101262b
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-10-16 22:22:57 +0300

    6.1. Классический поиск перебором.[#184749]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back1(int[] array) {
+        for (int index = 0; index != array.length/2; index++) {
+            int temp = array[index];
+            array[index]= array[array.length-index -1] ;
+            array[array.length-index -1]= temp;
+        }
+        return array;
+    }
+
+    public int[] back(int[] array) {
+        for (int index = 0; index != (array.length-1)/2; index++) {
+            int temp = array[index];
+            array[index]= array[array.length-index -1] ;
+            array[array.length-index -1]= temp;
+        }
+        return array;
+    }
+}
