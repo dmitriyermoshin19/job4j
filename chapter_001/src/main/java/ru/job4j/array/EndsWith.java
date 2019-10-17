1b0916479d13321a3fc554f0cf5e50c7e70181d8
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-10-17 16:39:04 +0300

    6.4.1. Слово заканчивается с ...[#184759]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        // проверить. что массив word имеет последние элементы одинаковые с post
+        for (int index = 0; post.length >  index; index++) {
+            if (word[(word.length-1)-index] == post[(post.length-1)-index]) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
