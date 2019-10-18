de831bd8e4afbcb7fa77bcd9de5d81ea5294e1ad
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-10-18 15:43:09 +0300

    Commit an home

--- chapter_001/src/main/java/ru/job4j/probnik/Masiv.java
+++ chapter_001/src/main/java/ru/job4j/probnik/Masiv.java
@@ -1,16 +1,25 @@
 package ru.job4j.probnik;
-
+/*
 public class Masiv {
+    public static int[] show(int[] array) {
+        int index ;
+            for ( index=0; index!=array.length; index++ ) {
+                int a = array[index];
+            }
+        return System.out.println(a);
+    }
+}
+
     public static void main(String[] args) {
-      /*  int[] n = {1, 2, 3};
+        int[] n = {1, 2, 3};
         for (int index = 0; index!=3; index++ ) {
             int a = n[index];
             System.out.println(a);
-        }*/
+        }
         int[] n = {1, 2, 3};
         for (int index = 0; index != n.length; index++) {
             int d = n[index];
             System.out.println(d); // Arrays.toString(n)
         }
     }
-}
+}*/
