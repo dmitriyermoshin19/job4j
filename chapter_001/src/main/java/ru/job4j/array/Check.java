1c27b02caa554b0af13a08f37907149753294216
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-10-19 12:49:03 +0300

    class Check

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 0; data.length != index; index++) {
             if (data[index] != data[0]) {
                  result = false;
                 break;
-            } else {
-                result =true;
             }
         }
         return result;
