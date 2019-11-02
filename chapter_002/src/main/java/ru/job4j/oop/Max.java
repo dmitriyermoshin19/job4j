ee664fa8e1b60e7fdf1833c3d9fe4fad42928d0a
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-11-02 14:40:29 +0300

    3. Перегрузить метод max для трех чисел[#184875]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Max {
+    public static int max(int first, int second) {
+        int result = first >= second ? first : second;
+        return result;
+    }
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first, max(second, third, fourth));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2To3Then3() {
+        int result = Max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenMax5To2To3Then5() {
+        int result = Max.max(5, 2, 3);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void whenMax5To2To3To4Then5() {
+        int result = Max.max(5, 2, 3, 4);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void whenMax1To2To3To4Then5() {
+        int result = Max.max(1, 2, 3, 5);
+        assertThat(result, is(5));
+    }
+}
