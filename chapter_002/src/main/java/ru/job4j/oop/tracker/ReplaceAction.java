46b317141e63ce3e5062aa2eef3038e45d596555
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-11-20 00:47:33 +0300

    ***

---
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/ReplaceAction.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop.tracker;
+
+public class ReplaceAction implements UserAction {
+    @Override
+    public String name() {
+        return "=== Edit item ===";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.print("Enter id: ");
+        String id = input.askStr("Enter id: ");
+        System.out.print("Edit name: ");
+        String name = input.askStr("");
+        Item item = new Item(name);
+        tracker.replace(id, item);
+        System.out.print("Item edited ");
+        return true;
+    }
+}
