5199e1cbb8d4a26ec74883702cd56b53c89bf3e0
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-11-19 19:15:15 +0300

    переделка тестов StartUITest после вывода методов.

---
+++ chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
@@ -0,0 +1,41 @@
+package ru.job4j.oop.tracker;
+
+import org.junit.Test;
+import ru.job4j.oop.tracker.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartUITest {
+    @Test
+    public void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        new CreateAction().execute(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
+    }
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId(), "replaced item"};
+        new ReplaceAction().execute(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        new DeleteAction().execute(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        Item rst = null;
+        assertThat(replaced, is(rst));
+    }
+}
