5536901221e333ae6df6e92240a25bbff530c8ea
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-12-16 21:39:03 +0300

    1. Телефонный справочник на базе ArrayList[#184820]

---
+++ chapter_003/src/main/java/ru/job4j/collection/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person per : persons) {
+            if (per.getName().contains(key) || per.getSurname().contains(key) || per.getPhone().contains(key) || per.getAddress().contains(key)) {
+                result.add(per);
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collection/search/PhoneDictionaryTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> result = phones.find("Petr");
+        assertThat(result.get(0).getSurname(), is("Arsentev"));
+    }
+}
