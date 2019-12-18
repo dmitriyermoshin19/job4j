2e049747a9c278e874748448400604a688cbc16f
dmitriyermoshin19 <dmstrannik@yandex.ru>
2019-12-18 19:43:43 +0300

    1. Конвертация листа массивов в один лист Integer[#184826]

---
+++ chapter_003/src/main/java/ru/job4j/collection/collection/ConvertListArray2List.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ConvertListArray2List {
+    public List<Integer> toList(List<int[]> listArray) {
+        List<Integer> list = new LinkedList<>();
+        int index = 0;
+        for (int[] row : listArray) {
+            for (int cell : row) {
+                list.add(cell);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<int[]> listArray = new LinkedList<>();
+        listArray.add(new int[]{1, 2});
+        listArray.add(new int[]{3, 4, 5, 6});
+        List<Integer> rsl = new ConvertListArray2List().toList(listArray);
+        for (Integer row : rsl) {
+            System.out.print(row + " ");
+        }
+    }
+}
