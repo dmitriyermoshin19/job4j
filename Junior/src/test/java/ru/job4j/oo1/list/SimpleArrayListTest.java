3d8ef37c15eba9f64669edc8ea96bf85eec50494
dmitriyermoshin19 <dmstrannik@yandex.ru>
2020-01-29 21:44:34 +0300

    5.3.0 Создать метод delete для односвязного списка[#225708]

---
+++ Junior/src/test/java/ru/job4j/oo1/list/SimpleArrayListTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.oo1.list;
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleArrayListTest {
+    private SimpleArrayList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new SimpleArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(1), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(list.getSize(), is(3));
+    }
+
+    @Test
+    public void whenDeleteFirstElementIsTwo() {
+        assertThat(list.delete(), is(3));
+        assertThat(list.get(0), is(2));
+    }
+
+    @Test
+    public void whenDeleteFirstElementThenUseGetSizeResultTwo() {
+        assertThat(list.getSize(), is(3));
+        list.delete();
+        assertThat(list.getSize(), is(2));
+    }
+}
