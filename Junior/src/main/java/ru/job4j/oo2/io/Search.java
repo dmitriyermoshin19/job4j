9d7ebba2115d8c19ca6542100fc519757bc5090f
dmitriyermoshin19 <dmstrannik@yandex.ru>
2020-02-27 23:17:58 +0300

    3. Сканирование файловой системы.[#225744]

---
+++ Junior/src/main/java/ru/job4j/oo2/io/Search.java
@@ -0,0 +1,26 @@
+package ru.job4j.oo2.io;
+
+import java.io.File;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Search {
+
+    /**
+     * @param parent - это путь до каталога, с которого нужно осуществлять поиск.
+     * @param predicate - это расширения файлов, которые мы хотим получить.
+     * @return List<File> подходящих хайлов.
+     */
+    public List<File> files(String parent, Predicate<File> predicate) {
+        List<File> list = new ArrayList<>();
+        File file = new File(parent);
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            if (f.isDirectory()) {
+                list.addAll(files(f.toString(), predicate));
+            } else if (predicate.test(f)) {
+                list.add(f);
+            }
+        }
+        return list;
+    }
+}
---
+++ Junior/src/test/java/ru/job4j/oo2/io/SearchTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.oo2.io;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+public class SearchTest {
+
+    @Test
+    public void myFolderTesting() {
+        String path = "./src/main";
+        Search search = new Search();
+        List<File> rsl = search.files(path, f -> {
+            List<String> ext = List.of("txt", "java", "properties", "csv");
+            return ext.contains(f.getName().substring(f.getName().indexOf(".") + 1));
+        });
+        assertThat(rsl.contains(new File("./src/main/java/ru/job4j/oo1/map/Analize.java")), is(true));
+        assertThat(rsl.contains(new File("./src/main/java/ru/job4j/oo1/map/SimpleHashMap.java")), is(true));
+        assertThat(rsl.contains(new File("./src/main/java/ru/job4j/oo1/map/app.prop")), is(false));
+        assertThat(rsl.size(), is(29));
+        rsl.forEach(System.out::println);
+    }
+}
