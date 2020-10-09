a1daddc545d9a8d37fbef8a0c8df41d7139208ed
dmitriyermoshin19 <dmstrannik@yandex.ru>
2020-10-09 21:41:07 +0300

    3.1. Прерывание блокированной нити.[267413#378386]

--- Middle/src/main/java/ru/job4j/oo1multithreading/threads/ThreadStop1.java
+++ Middle/src/main/java/ru/job4j/oo1multithreading/threads/ThreadStop1.java
@@ -1,26 +1,23 @@
 package ru.job4j.oo1multithreading.threads;
 
-public class Wait {
+public class ThreadStop1 {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(
                 () -> {
-                    int i = 1;
                     while (!Thread.currentThread().isInterrupted()) {
                         try {
-                            Thread.sleep(1000);
-                            System.out.print("\rLoading : " + i + "%");
+                            System.out.println("start ...");
+                            Thread.sleep(10000);
                         } catch (InterruptedException e) {
-                            System.out.println();
                             System.out.println(Thread.currentThread().isInterrupted());
                             System.out.println(Thread.currentThread().getState());
                             Thread.currentThread().interrupt();
                         }
-                        i++;
                     }
                 }
         );
         progress.start();
-        Thread.sleep(10000);
+        Thread.sleep(1000);
         progress.interrupt();
         progress.join();
     }
