e37358113b80e8f682fc7a8d797c282197bcc01a
dmitriyermoshin19 <dmstrannik@yandex.ru>
2020-11-06 00:48:55 +0300

    1. Неблокирующий кеш[4741#378405]

---
+++ Middle/src/main/java/ru/job4j/oo1multithreading/nonblockingalgoritm/Base.java
@@ -0,0 +1,36 @@
+package ru.job4j.oo1multithreading.nonblockingalgoritm;
+
+public class Base {
+    private int id;
+    private int version;
+    private String name;
+
+    public Base(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
