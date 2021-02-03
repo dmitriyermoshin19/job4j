1eae31db80dcb7d191478d513d03475046ff5cff
dmitriyermoshin19 <dmstrannik@yandex.ru>
2021-02-03 17:55:54 +0300

    3. LazyInitializationexception[331987#378304]

---
+++ Middle/src/main/java/ru/job4j/oo3hibernate/mapping/manytoone/Model.java
@@ -0,0 +1,74 @@
+package ru.job4j.oo3hibernate.mapping.manytoone;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "model")
+public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @ManyToOne()
+    @JoinColumn(name = "marka_id")
+    private Marka marka;
+
+    public static Model of(String name, Marka marka) {
+        Model model = new Model();
+        model.name = name;
+        model.marka = marka;
+        return model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Marka getMarka() {
+        return marka;
+    }
+
+    public void setMarka(Marka marka) {
+        this.marka = marka;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Model model = (Model) o;
+        return id == model.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Model{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", marka=" + marka + '}';
+    }
+}
