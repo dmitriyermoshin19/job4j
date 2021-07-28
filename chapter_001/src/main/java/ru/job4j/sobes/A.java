b99b22f7725f2539eed62364890195b7dd7dd466
dmitriyermoshin19 <dmstrannik@yandex.ru>
2021-07-28 19:39:53 +0300

    Последовательность вывода в консоль

---
+++ chapter_001/src/main/java/ru/job4j/sobes/A.java
@@ -0,0 +1,65 @@
+package ru.job4j.sobes;
+
+public class A {
+    static int x = 0;
+    int y = 1;
+    {
+        System.out.println("A");
+    }
+    static {
+        System.out.println("A static block");
+    }
+
+    public void a() {
+        System.out.println("!!! a() called");
+    }
+
+    public static void as() {
+        System.out.println("!!! as() static");
+    }
+
+    public A() {
+        System.out.println("A constructor");
+    }
+
+    static class B extends A {
+        static int u = 8;
+
+        static class C extends B {
+
+            public C() {
+                System.out.println("C constructor");
+            }
+
+            {
+                System.out.println("C");
+            }
+
+            static {
+                System.out.println("C static block");
+            }
+
+            public static void main(String[] args) {
+                A.B.C b = new C();
+                b.a();
+                as();
+            }
+        }
+        {
+            System.out.println("B");
+        }
+        static {
+            System.out.println("B static block");
+        }
+
+        public B() {
+            System.out.println("B constructor");
+        }
+    }
+
+    public static void main(String[] args) {
+      B b = new B();
+    }
+}
+
+
